c7bf7cb796f3bb6f1a13b1e43c75fafcd2d5fffc
Qheuss <quentin.heusse@gmail.com>
2024-05-08 15:27:00 +0200

    test

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -6,7 +6,7 @@ const Aside = ({ data, setFilteredData }) => {
   return (
     <aside>
       <NavLink to={`/`}>
-        <img className="logo" src="../src/assets/images/logo.svg" alt="logo" />
+        <img className="logo" src="./assets/images/logo.svg" alt="logo" />
       </NavLink>
       <div className="aside__content">
         <h2>Game Library</h2>
